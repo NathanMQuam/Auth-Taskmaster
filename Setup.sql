2f71140ef02730a59db524d607d60721038a082c
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-04-14 13:50:02 -0600

    Tables init

---
+++ Setup.sql
@@ -0,0 +1,44 @@
+-- CREATE TABLE profiles (
+--    id VARCHAR(255) NOT NULL PRIMARY KEY,
+--    name VARCHAR(255),
+--    email VARCHAR(255) NOT NULL UNIQUE,
+--    picture VARCHAR(255)
+-- );
+-- CREATE TABLE boards (
+--    id INT NOT NULL AUTO_INCREMENT,
+--    creatorId VARCHAR(255) NOT NULL,
+--    public TINYINT NOT NULL,
+--    name VARCHAR(255),
+--    description VARCHAR(255),
+--    PRIMARY KEY (id)
+-- );
+-- CREATE TABLE todos (
+--    id INT NOT NULL AUTO_INCREMENT,
+--    creatorId VARCHAR(255) NOT NULL,
+--    boardId INT NOT NULL,
+--    name VARCHAR(255),
+--    description VARCHAR(255),
+--    PRIMARY KEY(id),
+--    FOREIGN KEY (boardId) REFERENCES boards(id) ON DELETE CASCADE
+-- );
+-- INSERT INTO
+--    boards (creatorId, public, name, description)
+-- VALUES
+--    (
+--       "nathan",
+--       0,
+--       "Nathan's test board",
+--       "Nathan's test board's description"
+--    );
+-- SELECT
+--    *
+-- FROM
+--    boards;
+-- INSERT INTO
+--    todos (creatorId, boardId, name, description)
+-- VALUES
+--    ("nathan", 1, "Todo #1", "The first test Todo");
+-- SELECT
+--    *
+-- FROM
+--    todos;
